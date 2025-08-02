5920daa8f289dafe865448dd3bd2dd35d95d1906
madhusudhan123-star <dmadhusudhan98@gmail.com>
2025-08-02 15:29:01 +0530

    added

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'https://glowglaz.com',
-    'https://www.dralcofree.com',
+    'https://mother-nu.vercel.app',
     'http://localhost:3000'
   ],
   methods: ['GET', 'POST', 'OPTIONS'],
